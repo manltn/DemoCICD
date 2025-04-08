d65eaa0987384c017bc27e3199d4c07fa406a622
Mân Lê Thị Nha <manltnps38937@gmail.com>
2025-04-09 02:54:13 +0700

    test pass

--- src/main/java/com/demo/demoncicd/Caculator.java
+++ src/main/java/com/demo/demoncicd/Caculator.java
@@ -3,9 +3,9 @@ package com.demo.demoncicd;
 public class Caculator implements Icaculator{
 
     public Integer sum(Integer a, Integer b) {
-        return 0;
+        return a+b;
     }
     public Integer sub(Integer a, Integer b) {
-        return 0;
+        return a-b;
     }
 }
