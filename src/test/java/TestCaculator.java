717e957fbc120b20ac38ca2adcb1b60fcae5a248
Mân Lê Thị Nha <manltnps38937@gmail.com>
2025-04-09 03:04:40 +0700

    ✅ Fix test using TestNG

--- src/test/java/TestCaculator.java
+++ src/test/java/TestCaculator.java
@@ -1,19 +1,18 @@
 import com.demo.demoncicd.Caculator;
-import org.junit.jupiter.api.Test;
-import org.testng.Assert; // ✅
-
+import org.testng.annotations.Test;
+import org.testng.Assert;
 
 public class TestCaculator {
 
     @Test
     public void testSum() {
         Caculator caculator = new Caculator();
-        Assert.assertEquals(caculator.sum(2, 3), 5);
+        Assert.assertEquals(caculator.sum(2, 3), Integer.valueOf(5));
     }
 
     @Test
     public void testSub() {
         Caculator caculator = new Caculator();
-        Assert.assertEquals(caculator.sub(2, 2), 0);
+        Assert.assertEquals(caculator.sub(2, 2), Integer.valueOf(0));
     }
 }
