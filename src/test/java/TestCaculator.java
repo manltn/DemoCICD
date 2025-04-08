4c5d2a739f16c86317399dd6117f8e050ac2fb87
Mân Lê Thị Nha <manltnps38937@gmail.com>
2025-04-09 02:48:59 +0700

    fail test

--- src/test/java/TestCaculator.java
+++ src/test/java/TestCaculator.java
@@ -1,3 +1,19 @@
+import com.demo.demoncicd.Caculator;
+import org.junit.jupiter.api.Test;
+import org.testng.Assert; // ✅
+
+
 public class TestCaculator {
 
+    @Test
+    public void testSum() {
+        Caculator caculator = new Caculator();
+        Assert.assertEquals(caculator.sum(2, 3), 5);
+    }
+
+    @Test
+    public void testSub() {
+        Caculator caculator = new Caculator();
+        Assert.assertEquals(caculator.sub(2, 2), 0);
+    }
 }
